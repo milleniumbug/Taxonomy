e878dfd03dddaf4a5e2e7ae7c05616b8c5367213
milleniumbug <wojas1222@gmail.com>
2017-01-09 22:49:17 +0100

    added test project

---
+++ Tests/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	class ATest
+	{
+		[Test]
+		public void T()
+		{
+			Assert.AreEqual(2+2, 4);
+		}
+
+		static void Main()
+		{
+			
+		}
+	}
+}
