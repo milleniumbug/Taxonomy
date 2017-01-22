0f07171e6756d0fa458f659018f5d711b558cd21
milleniumbug <wojas1222@gmail.com>
2017-01-22 20:44:25 +0100

    fix analyzer inclusion in projects

--- TaxonomyMobile/TaxonomyMobile/App.xaml.cs
+++ TaxonomyMobile/TaxonomyMobile/App.xaml.cs
@@ -9,24 +9,24 @@ namespace TaxonomyMobile
 {
 	public partial class App : Application
 	{
-		public App ()
+		public App()
 		{
 			InitializeComponent();
 
 			MainPage = new TaxonomyMobile.MainPage();
 		}
 
-		protected override void OnStart ()
+		protected override void OnStart()
 		{
 			// Handle when your app starts
 		}
 
-		protected override void OnSleep ()
+		protected override void OnSleep()
 		{
 			// Handle when your app sleeps
 		}
 
-		protected override void OnResume ()
+		protected override void OnResume()
 		{
 			// Handle when your app resumes
 		}
