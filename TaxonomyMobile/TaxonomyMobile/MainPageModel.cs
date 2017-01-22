17408311be4dd89168252be7a31dd8963c8b5d1d
milleniumbug <wojas1222@gmail.com>
2017-01-22 19:02:07 +0100

    added TaxonomyMobile project for android

---
+++ TaxonomyMobile/TaxonomyMobile/MainPageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace TaxonomyMobile
+{
+    public class MainPageModel
+    {
+		public ObservableCollection<TaxonomyItem> Taxonomies { get; }
+
+	    public MainPageModel()
+	    {
+		    Taxonomies = new ObservableCollection<TaxonomyItem>() {new TaxonomyItem("asdf"), new TaxonomyItem("lol")};
+
+	    }
+	}
+}
---
+++ TaxonomyMobile/TaxonomyMobile/TaxonomyItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using TaxonomyLib;
+using Xamarin.Forms;
+
+namespace TaxonomyMobile
+{
+    public class TaxonomyItem
+    {
+	    private readonly Lazy<Taxonomy> taxonomy;
+	    public Taxonomy Taxonomy => taxonomy.Value;
+
+	    public string Path { get; }
+
+	    public TaxonomyItem(string path)
+	    {
+		    Path = path;
+			taxonomy = new Lazy<Taxonomy>(() => new Taxonomy(Path));
+	    }
+    }
+
+	public class TaxonomyItemToStringConverter : IValueConverter
+	{
+		public static readonly TaxonomyItemToStringConverter Default = new TaxonomyItemToStringConverter();
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var taxonomyItem = (TaxonomyItem)value;
+			return taxonomyItem.Path;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
