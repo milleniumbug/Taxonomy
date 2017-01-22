929c1cd8f8fcdd6a601e0a21a8461498ab7c4d1c
milleniumbug <wojas1222@gmail.com>
2017-01-22 10:43:59 +0100

    whitespace fix

--- Common/LinqExtensions.cs
+++ Common/LinqExtensions.cs
@@ -6,28 +6,28 @@ using System.Threading.Tasks;
 
 namespace Common
 {
-    public static class LinqExtensions
-    {
-	    public static IEnumerable<IEnumerable<TItem>> SplitToChunks<TItem>(this IEnumerable<TItem> @this, int width)
-	    {
-		    using (var enumerator = @this.GetEnumerator())
-		    {
-			    int i = 0;
-			    var chunk = new List<TItem>(width);
-			    while (enumerator.MoveNext())
-			    {
-				    chunk.Add(enumerator.Current);
-				    ++i;
-				    if (i == width)
-				    {
-					    yield return chunk;
-					    chunk = new List<TItem>(width);
-					    i = 0;
-				    }
-			    }
-			    if (chunk.Count != 0)
-				    yield return chunk;
-		    }
-	    }
-    }
+	public static class LinqExtensions
+	{
+		public static IEnumerable<IEnumerable<TItem>> RollingWindow<TItem>(this IEnumerable<TItem> @this, int width)
+		{
+			using (var enumerator = @this.GetEnumerator())
+			{
+				int i = 0;
+				var chunk = new List<TItem>(width);
+				while (enumerator.MoveNext())
+				{
+					chunk.Add(enumerator.Current);
+					++i;
+					if (i == width)
+					{
+						yield return chunk;
+						chunk = new List<TItem>(width);
+						i = 0;
+					}
+				}
+				if (chunk.Count != 0)
+					yield return chunk;
+			}
+		}
+	}
 }
